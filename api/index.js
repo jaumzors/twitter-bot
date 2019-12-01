2a00a43959f18be507198f6a8ec9255bbca17162
Joao Lopes <jpaulodba@gmail.com>
2019-12-01 00:04:53 -0300

    adding health endpoint and health checks

--- api/index.js
+++ api/index.js
@@ -4,6 +4,13 @@ const port = 3000;
 const repo = require('./services/repository.js');
 const _ = require('lodash');
 
+app.get('/health', (req, res) => {
+  res.json({
+    statusCode: 200,
+    status: 'up'
+  });
+});
+
 app.get('/tweets-info', (req, res) => {
   repo.get((err, data) => {
     if (!err) {
@@ -20,7 +27,7 @@ app.get('/tweets-info', (req, res) => {
 repo.connect((err, data) => {
 
   if (!err) {
-    app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+    app.listen(port, () => console.log(`api started`))
   } else {
     console.log("Unable to connect to database.");
   }
